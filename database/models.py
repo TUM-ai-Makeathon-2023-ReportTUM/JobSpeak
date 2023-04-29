fb88b3ecbd714a7fe8c611273c854aa1a6e2b4aa
jdgalviss <jdgalviss@gmail.com>
2023-04-30 00:39:06 +0200

    database

---
+++ database/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String, unique=False, index=True) # unique=True
+    reports = relationship("Report", back_populates="owner")
+
+class Report(Base):
+    __tablename__ = "reports"
+
+    id = Column(Integer, primary_key=True, index=True)
+    report_name = Column(String)
+    summary = Column(String)
+    date = Column(String)
+    img_path = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="reports")
